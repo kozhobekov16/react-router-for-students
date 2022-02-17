784227855c81e5a40d98ebe8539010e524efb80c
zhakshylyk <zhakshylykkozhobekov@gmail.com>
2022-02-17 19:24:28 +0600

    all

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import About from './Pages/About';
-import Contacts from './Pages/Contacts';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home';
-import Header from './components/Header'
-import NotFound from './Pages/NotFound';
-function App() {
+import React, {useState} from 'react'
+const App = () => {
+  const [theme, setTheme] = useState(false)
+  const changeName = () => {
+    setTimeout(() => {
+      setTheme((theme) => {
+        return theme === false
+      })
+    }, 1000)
+  }
   return (
-    <BrowserRouter>
-    <Header/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/contacts' element={<Contacts/>}/>
-        <Route path='*' element={<NotFound/>}/>
-      </Routes>
-    </BrowserRouter>
-  );
+    <div className={theme ? 'dark' : 'white'}>
+      <button onClick={changeName}>Change theme</button>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nemo ducimus voluptatibus, modi, dolorem explicabo perferendis sit eveniet corrupti molestias delectus qui est velit porro magni quis optio animi! Quibusdam.
+      Voluptatibus aspernatur libero cumque dolore ipsam consequatur nostrum, corrupti molestiae officiis repellendus nobis facilis harum aliquam rerum modi nam sed nesciunt assumenda consequuntur? Et maiores quod odit numquam, eaque minima?
+      Ea atque odit eos mollitia numquam tenetur quia dolores voluptates fugit labore accusamus hic, placeat tempora accusantium nostrum optio officiis aliquid, voluptas consequuntur incidunt? Rerum possimus pariatur impedit corporis error.
+      Numquam, necessitatibus quo. Adipisci modi deleniti hic ratione rem ex voluptate rerum distinctio ipsam blanditiis cumque officia dolorem, laboriosam debitis soluta magni voluptatem laudantium impedit. Aut repudiandae iste vel voluptatem.
+      Libero quas ducimus quia aut esse deserunt nihil, sit, sunt molestiae velit possimus dolorem quisquam tenetur, unde distinctio magnam debitis nam maiores animi. Sunt cumque aspernatur sapiente itaque error maiores.
+      Quaerat cum sit, accusamus repudiandae hic voluptatum possimus. Amet aperiam ex reiciendis alias temporibus officiis tempore voluptatem maiores molestias eius excepturi, nemo suscipit error dolor, ipsum quasi! Esse, voluptates aperiam!
+      Dignissimos velit, reprehenderit minus quibusdam omnis rerum assumenda modi, voluptates odio voluptatibus accusamus illum culpa sunt sint provident, id vel quis perspiciatis corrupti voluptate earum quo delectus exercitationem! Dolorum, laudantium.
+      Corporis neque consectetur perferendis voluptate? Ad modi ratione labore perferendis nostrum adipisci, reprehenderit voluptates debitis vel fugiat! Exercitationem natus velit vel, temporibus consectetur excepturi pariatur! Autem numquam molestiae omnis ullam!
+      Impedit consequuntur minus facere, error nisi asperiores consectetur? Eaque reprehenderit rerum tenetur aliquam quis, atque, ab id delectus quia necessitatibus similique? Iusto nobis veritatis praesentium quibusdam soluta itaque. At, et.
+      Molestias fugiat quaerat debitis esse quo at ex. Facilis quos, voluptates hic dignissimos voluptatem, non, laudantium repudiandae adipisci suscipit asperiores assumenda eligendi quae commodi totam provident illum! Quibusdam, reprehenderit officia.</p>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/Pages/Contacts.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Contacts = () => {
-  return (
-    <div>Contacts page</div>
-  )
-}
-
-export default Contacts
